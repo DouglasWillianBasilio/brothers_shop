0d881dac53822f1b6a0179e1dc201b488b2f4ae2
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-07-11 10:16:26 -0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { styled } from 'styled-components'
-import { Search } from "@material-ui/icons";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from '@mui/material';
 
 const Container = styled.div`
     height: 60px;
@@ -9,11 +10,14 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 `
 
 const Left = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
 `;
 
 const Language = styled.span`
@@ -22,14 +26,38 @@ const Language = styled.span`
 `;
 
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
-`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none;
+`;
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
+
+const Logo = styled.h1`
+    font-weight: bold;
+`;
+
+
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
 
 const Navbar = () => {
@@ -39,12 +67,21 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        input
-                        <Search />
+                        <Input />
+                        <Search style={{color:"gray", fontSize:16}}/>
                     </SearchContainer>
                 </Left>
-                <Center>Center</Center>
-                <Right>Right</Right>
+                <Center><Logo>BROTHERS ELETRÔNICA &copy;</Logo></Center>
+                <Right>
+
+                    <MenuItem>REGISTRAR</MenuItem>
+                    <MenuItem>ENTRAR</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
             </Wrapper>
 
         </Container>
